db9105517886e7ab45772333b117c656ff8c4d6f
RafiZR <148331488+RafiZR@users.noreply.github.com>
2023-10-24 10:58:46 +0700

    Add files via upload

---
+++ pertemuan 1/Bola.py
@@ -0,0 +1,15 @@
+print("Menghitung Luas dan Volume Bola")
+
+#variabel 
+pi = 22/7
+jari = 7
+
+#Rumus
+luas   = 4*pi*(jari*jari)
+volume = 4/3*pi*(jari*jari*jari)
+
+#Output
+print("pi :", pi)
+print("jari :", jari)
+print("luas :", luas)
+print("volume:", volume)
